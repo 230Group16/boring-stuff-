420e5f60dcebd620ab6e21c41c12be0ee8fb4ef5
RaBind <rabindra555@hotmail.com>
2015-11-23 18:05:34 +0000

    Update and rename FakeScoreboard.java to Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -15,21 +15,29 @@ public class Scoreboard extends JPanel {
 	      graphics.setColor(new Color(COLOR_COMPONENT, 
 	                                  COLOR_COMPONENT,
 	                                  COLOR_COMPONENT ) );
-	      graphics.fillRect( FIRST_X_COORD,
-	                         FIRST_Y_COORD,
-	                         BAR_WIDTH,
-	                         BAR_LENGTH );
+	      graphics.fillRect(FIRST_X_COORD,
+	    		  			FIRST_Y_COORD,
+	    		  			BAR_WIDTH,
+	    		  			BAR_HEIGHT);
 	      
 	      /* set new drawing color using predefined variable */
 	      graphics.setColor(Color.BLACK);
 	      graphics.drawString( "Crap", TEXT_X_COORD, TEXT_Y_COORD );
 	   } // end method paintComponent
 	
+	public int getWidth(){
+		return BAR_WIDTH;
+	}
+	
+	public int getHeight(){
+		return BAR_HEIGHT;
+	}
+	
 	private static final int COLOR_COMPONENT = 255;
 	private static final int FIRST_X_COORD = 0;
 	private static final int FIRST_Y_COORD = 0;
-	private static final int BAR_LENGTH = 200;
 	private static final int BAR_WIDTH = 700;
+	private static final int BAR_HEIGHT = 200;
 	private static final int TEXT_X_COORD = 30;
 	private static final int TEXT_Y_COORD = 30;
 }
