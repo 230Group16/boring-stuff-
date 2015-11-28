5412bfbb596af3c51a8bab792c9a5f07f2a3f9dc
RaBind <rabindra555@hotmail.com>
2015-11-28 17:29:49 +0000

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,15 +1,20 @@
 package kablewie;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Graphics;
 import java.util.Calendar;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 public class Scoreboard extends JPanel {
+	
 	Calendar m_gameTime;
 	int m_minesDiffused;
 	int m_numberOfTilesRevealed;
+	
 	
 	public void update() {
 		
@@ -28,8 +33,8 @@ public class Scoreboard extends JPanel {
 	}
 	
 	public void paintComponent( Graphics graphics ) {
-
-	      /* call superclass's paintComponent */
+		  
+		  /* call superclass's paintComponent */
 	      super.paintComponent( graphics );
 
 	      /* set new drawing color using integers */
@@ -38,14 +43,37 @@ public class Scoreboard extends JPanel {
 	                                  COLOR_COMPONENT ) );
 	      graphics.fillRect(FIRST_X_COORD,
 	    		  			FIRST_Y_COORD,
-	    		  			BAR_WIDTH,
-	    		  			BAR_HEIGHT);
+	    		  			getWidth(),
+	    		  			getHeight());
 	      
 	      /* set new drawing color using predefined variable */
 	      graphics.setColor(Color.BLACK);
 	      graphics.drawString( "Scoreboard goes here.", TEXT_X_COORD, TEXT_Y_COORD );
-	   } // end method paintComponent
+	      
+	      /*Sets spring latout for scoreboard */
+	      SpringLayout layout = new SpringLayout();
+	      this.setLayout(layout);
+	      
+	      
+	      this.addComponent(namelbl);
+	      
+	      layout.putConstraint(SpringLayout.WEST, namelbl,
+                  5,
+                  SpringLayout.WEST, this);
+	      layout.putConstraint(SpringLayout.NORTH, namelbl,
+                  5,
+                  SpringLayout.NORTH, this);
+	      
+	} // end method paintComponent
 	
+	private void addComponent(Component x){
+		this.add(x);
+	}
+	
+	/* Creates a label for the player's name */
+	JLabel namelbl = new JLabel("player's name" + getPlayername());
+	
+	/* Getter methods for weidth, height and player's name */
 	public int getWidth(){
 		return BAR_WIDTH;
 	}
@@ -54,11 +82,17 @@ public class Scoreboard extends JPanel {
 		return BAR_HEIGHT;
 	}
 	
+	public String getPlayername(){
+		return playername;
+	}
+	
+	/* Initialixation of variables */
+	private String playername = "";
 	private static final int COLOR_COMPONENT = 255;
 	private static final int FIRST_X_COORD = 0;
 	private static final int FIRST_Y_COORD = 0;
-	private static final int BAR_WIDTH = 700;
-	private static final int BAR_HEIGHT = 200;
+	private static final int BAR_WIDTH = Game.getMBarWidth();
+	private static final int BAR_HEIGHT = 100;
 	private static final int TEXT_X_COORD = 30;
 	private static final int TEXT_Y_COORD = 30;
 }
