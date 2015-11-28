a66ed1a195ba540f238b45f5f98fe4ad0d7fcef4
RaBind <rabindra555@hotmail.com>
2015-11-28 17:29:18 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,14 +1,12 @@
 /**
- * @file    -TextFieldFrameProper.java
- * @author  -P.J. Deitel, H.M. Deitel and R.S. Laramee
- * @date    -6 Dec '10
- * @see     -Deitel and Deitel, Fig. 11.9, page 394
+ * @file    -Game.java
+ * @author  -Rabidra Thapa
+ * @date    -28/11/2015
+ * @see     -Board.java
+ * @see		-Scoreboard.java
  *
- *  \brief A simple Java Swing Example that demonstrates
- *  the JTextField class.
- *
- *  This example is an improvement over TextFieldFrameCrappy.
- *  There are many small but very important differences.
+ * A simple frame that can contain panels
+ * 
  */
 
 package kablewie;
@@ -16,30 +14,45 @@ package kablewie;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
-
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class Game {
 	
+	/* *
+     * Get method for frame Height 
+     * @return Frame's height
+     */
+	public static int getMBarHeight() {
+		return BAR_HEIGHT;
+	}
+	
+	/* *
+     * Get method for frame Width 
+     * @return Frame's width
+     */
+	public static int getMBarWidth() {
+		return BAR_WIDTH;
+	}
+	
+	
     public static void main( String args[] ) {
-    	
+    
     	/* create frame for Board and Scoreboard */
     	JFrame frame = new JFrame( "Klablewie!!!");
     	frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     	
+    	/* create container */
     	Container pane = frame.getContentPane();
     	
+    	/* set container layout to boxlayout */
     	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
     	
-    	
-    	
-    	/* create Board and Scoreboard panels */
+    	/* creates Board and Scoreboard panels and sets their verticle alignmenrt and preferred sizes */
     	Scoreboard scoreboardpanel = new Scoreboard();
     	scoreboardpanel.setAlignmentX(Component.CENTER_ALIGNMENT);
     	scoreboardpanel.setPreferredSize(new Dimension(scoreboardpanel.getWidth(), scoreboardpanel.getHeight()));
+    	
     	Board boardpanel = new Board();
     	boardpanel.setAlignmentX(Component.CENTER_ALIGNMENT);
     	boardpanel.setPreferredSize(new Dimension(boardpanel.getWidth(), boardpanel.getHeight()));
@@ -52,12 +65,17 @@ public class Game {
     	pane.add(scoreboardpanel);
     	pane.add(boardpanel);
     	
-    	frame.pack();
+    	/* Sets frame size */
+    	frame.setSize(BAR_WIDTH, BAR_HEIGHT);
+    	
     	/* display frame */
         frame.setVisible(true);
         
         /* set frame resizable to false */
-        frame.setResizable(true);
+        frame.setResizable(false);
 	}
+	
+	private static final int BAR_WIDTH = 700;
+	private static final int BAR_HEIGHT = 700;
     
 }
