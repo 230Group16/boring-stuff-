a8b7b6edd145abc3653047cf12990e0b80ef01f3
Aledrj <aled95@hotmail.com>
2015-12-03 13:04:16 +0000

    Nice indentation

--- Mine.java
+++ Mine.java
@@ -2,16 +2,16 @@ package kablewie;
 
 
 public class Mine extends Tile {
-	
-	public Mine(int x, int y){
-		super(x,y);
-	}
-	
-	public boolean hasMine(){
-		return true;
-	}
-	
-	public void m_showGraphic(int mineCount){
-		super.m_showGraphic(mineCount);
-	}
+    
+    public Mine(int x, int y){
+        super(x,y);
+    }
+    
+    public boolean hasMine(){
+        return true;
+    }
+    
+    public void m_showGraphic(int mineCount){
+        super.m_showGraphic(mineCount);
+    }
 }
