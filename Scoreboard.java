d7084b649e8b3624dd1bd4f74619ab0efcfbbbb8
Tom2656 <thomas.fisher1@ntlworld.com>
2015-12-05 20:16:31 +0000

    Timer fixed and removed unnecessary code
    Removed paintComponent method and made timer work again.

--- Scoreboard.java
+++ Scoreboard.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -37,39 +38,12 @@ public class Scoreboard extends JPanel {
         return m_numberOfTilesRevealed;
     }
     
-    public void paintComponent( Graphics graphics ) {
-        
-        /* call superclass's paintComponent */
-        super.paintComponent( graphics );
-        
-        /* set new drawing color using integers */
-        graphics.setColor(new Color(COLOR_COMPONENT,
-        COLOR_COMPONENT,
-        COLOR_COMPONENT ) );
-        graphics.fillRect(FIRST_X_COORD,
-        FIRST_Y_COORD,
-        getWidth(),
-        getHeight());
-        
-        /* set new drawing color using predefined variable */
-        graphics.setColor(Color.BLACK);
-        graphics.drawString( "Scoreboard goes here.", TEXT_X_COORD, TEXT_Y_COORD );
-        
-        /*Sets spring latout for scoreboard */
-        SpringLayout layout = new SpringLayout();
-        this.setLayout(layout);
-        
-        
-        this.addComponent(namelbl);
-        this.addComponent(gTimelbl);
-        
-        layout.putConstraint(SpringLayout.WEST, namelbl,
-        5,
-        SpringLayout.WEST, this);
-        layout.putConstraint(SpringLayout.NORTH, namelbl,
-        5,
-        SpringLayout.NORTH, this);
-        
+    public Scoreboard() {
+        /*Sets grid layout for scoreboard */
+        this.setLayout(new GridLayout(3,1));
+ 
+        this.addComponent(lblName);
+        this.addComponent(lblTime);
     }
     
     private void addComponent(Component x){
@@ -85,10 +59,10 @@ public class Scoreboard extends JPanel {
 	}
 	
     public void update() {
-	      gTimelbl.setText("Game time: " + getGTime());
+	      lblTime.setText("Game time: " + getGTime());
 	}
     
-    /*public static void updatetime(){
+    public static void updatetime(){
 		//Creates a timer and sets a task to iterate m_time at every second
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask()
@@ -99,18 +73,13 @@ public class Scoreboard extends JPanel {
              	 System.out.println(Scoreboard.m_time);
              }
         }, MS_IN_SECOND, MS_IN_SECOND);
-	}*/
+	}
 	
     /* Initialisation of variables */
     private String m_playerName = "";
     /* Creates a label for the player's name */
-    JLabel namelbl = new JLabel("player's name: " + m_playerName);
+    JLabel lblName = new JLabel("Player's Name: " + m_playerName);
     /* Creates a label for the player's name */
-	JLabel gTimelbl = new JLabel("Game time: " + getGTime());
-    private static final int COLOR_COMPONENT = 255;
-    private static final int FIRST_X_COORD = 0;
-    private static final int FIRST_Y_COORD = 0;
-    private static final int TEXT_X_COORD = 30;
-    private static final int TEXT_Y_COORD = 30;
+	JLabel lblTime = new JLabel("Game time: " + getGTime());
     final static long MS_IN_SECOND = 1000L;
 }
