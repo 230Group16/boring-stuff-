d4aa0d59900c83512e41980a420d7a483888f402
Tom2656 <thomas.fisher1@ntlworld.com>
2015-12-05 22:52:10 +0000

    Can now make new games
    Added endGame method so that games end when a mine is clicked. Can now make new games using makeGame().

--- Game.java
+++ Game.java
@@ -12,6 +12,8 @@
 package kablewie;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Timer;
 
 import javax.swing.*;
@@ -45,32 +47,52 @@ public class Game extends JPanel {
     
     
     public Game(int size, int mines) {
-    	m_board = new Board(size, mines);
-    	m_scoreboard = new Scoreboard();
-    	
-    	m_height = m_board.getSideLength() + SCOREBOARD_HEIGHT;
-    	m_width = m_board.getSideLength();
-    	
-    	m_scoreboard.setPreferredSize(new Dimension(m_width, SCOREBOARD_HEIGHT));
-    	
-    	//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-    	/* add boardPanel and scoreboardPanel to frame */
-    	
-    	add(m_scoreboard);
+        makeGame(size, mines);
+    }
+    
+    public void makeGame(int s, int m){
+        m_board = new Board(s, m);
+        m_scoreboard = new Scoreboard();
+        
+        m_height = m_board.getSideLength() + SCOREBOARD_HEIGHT;
+        m_width = m_board.getSideLength();
+        
+        m_scoreboard.setPreferredSize(new Dimension(m_width, SCOREBOARD_HEIGHT));
+        
+        //setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        /* add boardPanel and scoreboardPanel to frame */
+        
+        add(m_scoreboard);
         add(m_board);
 
         setPreferredSize(new Dimension(m_width, m_height));
+        updateUI();
     }
     
-    public void endGame(boolean tf) {
-    	//Stop timer
-    	removeAll();
+    public void endGame(char gameState) {    
+    	GameContainer.stop();
     	
-    	if (tf) {
-    		//Win
-    	} else {
-    		//Loss and display animation
+    	switch(gameState){
+        case 'w':
+            //Win
+            
+            break;
+        case 'l':
+            //Loss and display animation
+            m_scoreboard.setGameStateMessage("You lose!");
+            break;
+        case 'r':
+            //reset
+            break;
+        }
+        
+    	for (int i=0; i<m_board.getBoardSize(); i++){
+    		for (int j=0; j<m_board.getBoardSize();j++) {
+    			m_board.getTile(j, i).setEnabled(false);
+    			m_board.getTile(j, i).setDisabledIcon(m_board.getTile(j, i).getIcon());
+    		}
     	}
+        
     }
 
     
