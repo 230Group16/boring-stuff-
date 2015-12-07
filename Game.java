bb952ff617f77c5aee561f053f419814af535d10
therealbaylife <benh0795@gmail.com>
2015-12-07 05:04:58 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,11 +1,12 @@
 /**
 * @file    -Game.java
-* @author  -Rabidra Thapa
+* @author  -Ben Harris, Thomas Fisher, David Jones
 * @date    -28/11/2015
+* @see	   -GameContainer.java
 * @see     -Board.java
-* @see	    -Scoreboard.java
+* @see	   -Scoreboard.java
 *
-* A simple frame that can contain panels
+* A simple panel that can contains the core of the game
 *
 */
 
@@ -16,42 +17,89 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 public class Game extends JPanel {
+	public static final int SCOREBOARD_HEIGHT = 100; 
 	
-	public int getHeight() {
+	private Board m_board;
+    private Scoreboard m_scoreboard;
+	private Human m_player;
+	
+    private int m_height;
+    private int m_width;
+   
+    /**
+	 * Getter method returning the height of the game panel
+	 * @return m_height (Game height in pixels)
+	 */
+    public int getHeight() {
 		return m_height;
 	}
 	
+	/**
+	 * Getter method returning the width of the game panel
+	 * @return m_width (Game width in pixels)
+	 */
 	public int getWidth() {
 		return m_width;
 	}
 	
+	/**
+	 * Getter method returning the Board panel (a child of Game)
+	 * @return m_board (the board contained in Game)
+	 */
 	public Board getBoard() {
 		return m_board;
 	}
 	
+	/**
+	 * Getter method returning the Scoreboard panel (a child of Game)
+	 * @return m_scoreboard (the scoreboard contained in game)
+	 */
 	public Scoreboard getScoreboard() {
 		return m_scoreboard;
 	}
 	
+	/**
+	 * Getter method returning the Human object
+	 * @return m_player (the player object)
+	 */
 	public Human getPlayer() {
 		return m_player;
 	}
 	
+	/**
+	 * Constructor for this class. Takes in two args for use in makeGame()
+	 * @param size The width and height of the board (measured in tiles)
+	 * @param mines The amount of mines to be added to the board
+	 * @see makeGame(int, int)
+	 */
+	public Game(int size, int mines) {
+        makeGame(size, mines);
+    }
+	
+	/**
+	 * Method that takes a string and uses it to create
+	 * a human object as well as set the name on the scoreboard
+	 * @param name a string argument used to set name and initialise Human
+	 * @see Scoreboard.java
+	 * @see Human.java
+	 * @return a new Human object, using name and this (Game) as parameters
+	 */
 	public Human newPlayer(String name) {
     	m_scoreboard.setPlayerName(name);
     	return new Human(name, this);
     }
 	
-    public Game(int size, int mines) {
-        makeGame(size, mines);
-    }
-    
-    
-    
+	/**
+	 * The primary method for this class that takes in two integers and
+	 * uses them to create the core of the game (The board and the scoreboard)
+	 * @param s Size of board measured in tiles
+	 * @param m Amount of mines to be placed into the board
+	 * @see Board.java
+	 * @see Scoreboard.java
+	 */
     public void makeGame(int s, int m){
     	m_board = new Board(s, m);
         m_scoreboard = new Scoreboard(this);
@@ -61,7 +109,6 @@ public class Game extends JPanel {
         
         m_scoreboard.setBackground(Color.WHITE);
         
-        /* add board and scoreboard to frame */
         add(m_scoreboard);
         add(m_board);
         
@@ -72,15 +119,22 @@ public class Game extends JPanel {
         updateUI();
     }
     
+	/**
+	 * This method ends the game in one of three ways - Reset, Win or Loss
+	 * with the character being r for reset, w for win and l for loss.
+	 * Upon loss also adds the loss animation to the game
+	 * @param gameState A character representation for each state
+	 * @see Scoreboard.setGameState(String)
+	 * @see Scoreboard.stopTimer()
+	 * @see Animations.java
+	 */
     public void endGame(char gameState) {    
 
     	switch(gameState){
         case 'w':
-            //Win
         	m_scoreboard.setGameState("You win!");
             break;
         case 'l':
-            //Loss and display animation
             m_scoreboard.setGameState("You lose!");
             Animations a = new Animations(this); 
             add(a);
@@ -92,21 +146,5 @@ public class Game extends JPanel {
             m_scoreboard.reset();
             break;
         }
-    	 
-		for (int i=0; i<m_board.getBoardSize(); i++){
-	    	for (int j=0; j<m_board.getBoardSize();j++) {
-	    		m_board.reveal(m_board.getTile(j, i));
-	    	}
-		}
-		
-        
     }
-    
-	
-    private Board m_board;
-    private Scoreboard m_scoreboard;
-    public static final int SCOREBOARD_HEIGHT = 100; 
-    private int m_height;
-    private int m_width;
-    private Human m_player;
 }
