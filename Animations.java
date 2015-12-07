3f86de7aef6d72589f19f8f004c9c29fabc87d3e
Tom2656 <thomas.fisher1@ntlworld.com>
2015-12-07 02:37:05 +0000

    Added comments

--- Animations.java
+++ Animations.java
@@ -9,41 +9,65 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+/**
+* @file Animations.java
+* @author David
+* @date 07 Dec 2015
+* @see Game.java, Board.java for related information.
+*
+* Creates the losing animation of Kablewie 
+*/
+
 
 	public class Animations extends JPanel implements ActionListener {
 		
 		public Game m_game;
-		private BufferedImage image;
-
+		private BufferedImage m_image;
+		
+		/**
+		 * A constructor taking one argument 
+		 * and returning a new instance of an animation,
+		 * @param g an instance of the object Game.class.
+		 * @return new Animations object
+		 */
 		public Animations(Game g) {
 			m_game = g;
 			try {
-				image = ImageIO.read(Game.class.getResourceAsStream("/images/kablewie.png"));
+				m_image = ImageIO.read(Game.class.getResourceAsStream("/images/kablewie.png"));
 			} catch  (IOException e) {
 				e.printStackTrace();
 			}
 		} 
 		
 		Timer t = new Timer(5, this);
-		int x = 150;
+		int x = 0;
 		int y = 150; 
-		double velX = 1, velY = 1;
+		double m_velX = 1, m_velY = 1;
 		
+		 
+	    /**
+		* A method that draws the image on the screen.
+		* @param g Graphics argument
+		*/
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.drawImage(image, x, y, null);
+			g.drawImage(m_image, x, y, null);
 			t.start();
 		}
 		
+		 /**
+		* A method that moves the image accross the screen.
+		* @param e ActionEvent argument 
+		*/
 		public void actionPerformed(ActionEvent e) {
-			if (x < 0 || x > 200) { 
-				velX = -velX;
+			if (x < 0 || x > (m_game.getWidth()-100)) { 
+				m_velX = -m_velX;
 			}
-			if (y < 0 || y > 250) { 
-				velY = -velY;
+			if (y < 0 || y > (m_game.getHeight()-135)) { 
+				m_velY = -m_velY;
 			}
-			x += velX;
-			y += velY; 
+			x += m_velX;
+			y += m_velY; 
 			repaint(); 
 		}
 	}
