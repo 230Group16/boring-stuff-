3dba86d1bb61d6fb9b49b6518df217b43125be3c
therealbaylife <benh0795@gmail.com>
2015-12-07 05:11:48 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -23,17 +23,16 @@ public class Game extends JPanel {
 	public static final int SCOREBOARD_HEIGHT = 100; 
 	
 	private Board m_board;
-    private Scoreboard m_scoreboard;
+	private Scoreboard m_scoreboard;
 	private Human m_player;
-	
-    private int m_height;
-    private int m_width;
+	private int m_height;
+	private int m_width;
    
-    /**
+    	/**
 	 * Getter method returning the height of the game panel
 	 * @return m_height (Game height in pixels)
 	 */
-    public int getHeight() {
+	public int getHeight() {
 		return m_height;
 	}
 	
@@ -77,7 +76,7 @@ public class Game extends JPanel {
 	 */
 	public Game(int size, int mines) {
         makeGame(size, mines);
-    }
+	}
 	
 	/**
 	 * Method that takes a string and uses it to create
@@ -88,9 +87,9 @@ public class Game extends JPanel {
 	 * @return a new Human object, using name and this (Game) as parameters
 	 */
 	public Human newPlayer(String name) {
-    	m_scoreboard.setPlayerName(name);
-    	return new Human(name, this);
-    }
+    		m_scoreboard.setPlayerName(name);
+		return new Human(name, this);
+	}
 	
 	/**
 	 * The primary method for this class that takes in two integers and
@@ -100,24 +99,24 @@ public class Game extends JPanel {
 	 * @see Board.java
 	 * @see Scoreboard.java
 	 */
-    public void makeGame(int s, int m){
-    	m_board = new Board(s, m);
-        m_scoreboard = new Scoreboard(this);
+	public void makeGame(int s, int m){
+    		m_board = new Board(s, m);
+        	m_scoreboard = new Scoreboard(this);
         
-        m_height = m_board.getHeight() + SCOREBOARD_HEIGHT;
-        m_width = m_board.getWidth();
+        	m_height = m_board.getHeight() + SCOREBOARD_HEIGHT;
+		m_width = m_board.getWidth();
         
-        m_scoreboard.setBackground(Color.WHITE);
+        	m_scoreboard.setBackground(Color.WHITE);
         
-        add(m_scoreboard);
-        add(m_board);
+        	add(m_scoreboard);
+		add(m_board);
         
-        setLayout(null);
-        m_scoreboard.setBounds(0,0, m_width, SCOREBOARD_HEIGHT);
-        m_board.setBounds(0,SCOREBOARD_HEIGHT, m_width, m_board.getHeight());
-        setPreferredSize(new Dimension(m_width, m_height));
-        updateUI();
-    }
+		setLayout(null);
+		m_scoreboard.setBounds(0,0, m_width, SCOREBOARD_HEIGHT);
+		m_board.setBounds(0,SCOREBOARD_HEIGHT, m_width, m_board.getHeight());
+		setPreferredSize(new Dimension(m_width, m_height));
+		updateUI();
+	}
     
 	/**
 	 * This method ends the game in one of three ways - Reset, Win or Loss
@@ -128,23 +127,23 @@ public class Game extends JPanel {
 	 * @see Scoreboard.stopTimer()
 	 * @see Animations.java
 	 */
-    public void endGame(char gameState) {    
+	public void endGame(char gameState) {    
 
-    	switch(gameState){
-        case 'w':
-        	m_scoreboard.setGameState("You win!");
-            break;
-        case 'l':
-            m_scoreboard.setGameState("You lose!");
-            Animations a = new Animations(this); 
-            add(a);
-            remove(m_board);
-            a.setBounds(0,SCOREBOARD_HEIGHT, m_width, m_board.getHeight());
-            m_scoreboard.stopTimer();
-            break;
-        case 'r':
-            m_scoreboard.reset();
-            break;
-        }
-    }
+		switch(gameState){
+        	case 'w':
+        		m_scoreboard.setGameState("You win!");
+            		break;
+        	case 'l':
+			m_scoreboard.setGameState("You lose!");
+			Animations a = new Animations(this); 
+			add(a);
+			remove(m_board);
+			a.setBounds(0,SCOREBOARD_HEIGHT, m_width, m_board.getHeight());
+			m_scoreboard.stopTimer();
+			break;
+		case 'r':
+			m_scoreboard.reset();
+			break;
+		}
+	}
 }
