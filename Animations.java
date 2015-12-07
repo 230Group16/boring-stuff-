ddc891505e37217b08ca5b488ddf1f4e0701c749
Calidia <stoyanova.tereza@gmail.com>
2015-12-07 09:18:39 +0000

    Update Animations.java

--- Animations.java
+++ Animations.java
@@ -20,62 +20,62 @@ import javax.swing.Timer;
 */
 
 
-	public class Animations extends JPanel implements ActionListener {
-		
-		public Game m_game;
-		private BufferedImage m_image;
-		
-		/**
-		 * A constructor taking one argument 
-		 * and returning a new instance of an animation,
-		 * @param g an instance of the object Game.class.
-		 * @return new Animations object
-		 */
-		public Animations(Game g) {
-			m_game = g;
-			try {
-				InputStream img = Game.class.getResourceAsStream(
-						"/images/kablewie.png");
-				m_image = ImageIO.read(Game.class.getResourceAsStream(
-						"/images/kablewie.png"));
-			} catch  (IOException e) {
-				e.printStackTrace();
-			}
-		} 
-		
-		Timer t = new Timer(5, this);
-		int x = 0;
-		int y = ANIMATION_START_Y; 
-		double m_velX = 1, m_velY = 1;
-		
-		 
-	    /**
-		* A method that draws the image on the screen.
-		* @param g Graphics argument
-		*/
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			g.drawImage(m_image, x, y, null);
-			t.start();
+public class Animations extends JPanel implements ActionListener {
+	
+	public Game m_game;
+	private BufferedImage m_image;
+	
+	/**
+	 * A constructor taking one argument 
+	 * and returning a new instance of an animation,
+	 * @param g an instance of the object Game.class.
+	 * @return new Animations object
+	 */
+	public Animations(Game g) {
+		m_game = g;
+		try {
+			InputStream img = Game.class.getResourceAsStream(
+					"/images/kablewie.png");
+			m_image = ImageIO.read(Game.class.getResourceAsStream(
+					"/images/kablewie.png"));
+		} catch  (IOException e) {
+			e.printStackTrace();
 		}
-		
-		 /**
-		* A method that moves the image accross the screen.
-		* @param e ActionEvent argument 
-		*/
-		public void actionPerformed(ActionEvent e) {
-			if (x < 0 || x > (m_game.getWidth()-BOUNCE_WIDTH)) { 
-				m_velX = -m_velX;
-			}
-			if (y < 0 || y > (m_game.getHeight()-BOUNCE_HEIGHT)) { 
-				m_velY = -m_velY;
-			}
-			x += m_velX;
-			y += m_velY; 
-			repaint(); 
-		}
-		
-		public static final int ANIMATION_START_Y = 150;
-		public static final int BOUNCE_WIDTH = 100;
-		public static final int BOUNCE_HEIGHT = 135;
+	} 
+	
+	Timer t = new Timer(5, this);
+	int x = 0;
+	int y = ANIMATION_START_Y; 
+	double m_velX = 1, m_velY = 1;
+	
+	 
+    	/**
+	 * A method that draws the image on the screen.
+	 * @param g Graphics argument
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(m_image, x, y, null);
+		t.start();
 	}
+	
+	 /**
+	* A method that moves the image accross the screen.
+	* @param e ActionEvent argument 
+	*/
+	public void actionPerformed(ActionEvent e) {
+		if (x < 0 || x > (m_game.getWidth()-BOUNCE_WIDTH)) { 
+			m_velX = -m_velX;
+		}
+		if (y < 0 || y > (m_game.getHeight()-BOUNCE_HEIGHT)) { 
+			m_velY = -m_velY;
+		}
+		x += m_velX;
+		y += m_velY; 
+		repaint(); 
+	}
+	
+	public static final int ANIMATION_START_Y = 150;
+	public static final int BOUNCE_WIDTH = 100;
+	public static final int BOUNCE_HEIGHT = 135;
+}
