56a49aff9e08ed14c2ece13cbfcb660d0b5a5e02
Tom2656 <thomas.fisher1@ntlworld.com>
2015-12-07 09:42:50 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,13 @@
 package kablewie;
 
-
+/**
+ * @file	-Player.java
+ * @author	-Neycho Neychev
+ * @date    -28/11/2015
+ * @see		-Human.java
+ *
+ * An abstract class that is extended by player types.
+ */
 public abstract class Player {
 	abstract void takeTurn();
 }
